0f47295724bc8f2b154260f175264e526f74ed74
obrailovska <olabrrai@icloud.com>
2022-04-19 13:45:39 -0400

    added comment model

--- controllers/pizza-controller.js
+++ controllers/pizza-controller.js
@@ -4,18 +4,31 @@ const pizzaController = {
   // get all pizzas
   getAllPizza(req, res) {
     Pizza.find({})
+      .populate({
+        path: "comments",
+        select: "-__v",
+      })
+      .select("-__v")
+      .sort({ _id: -1 })
       .then((dbPizzaData) => res.json(dbPizzaData))
       .catch((err) => {
         console.log(err);
         res.status(400).json(err);
       });
+
+    //   set up the query so that the newest pizza returns first. Mongoose has a .sort() method to help with this. After the .select() method, use .sort({ _id: -1 }) to sort in DESC order by the _id value. This gets the newest pizza because a timestamp value is hidden somewhere inside the MongoDB ObjectId.
+    //   Note that we also used the select option inside of populate(), so that we can tell Mongoose that we don't care about the __v field on comments either. The minus sign - in front of the field indicates that we don't want it to be returned. If we didn't have it, it would mean that it would return only the __v field.
   },
 
   // get one pizza by id
   getPizzaById({ params }, res) {
     Pizza.findOne({ _id: params.id })
+      .populate({
+        path: "comments",
+        select: "-__v",
+      })
+      .select("-__v")
       .then((dbPizzaData) => {
-        // If no pizza is found, send 404
         if (!dbPizzaData) {
           res.status(404).json({ message: "No pizza found with this id!" });
           return;
@@ -26,6 +39,7 @@ const pizzaController = {
         console.log(err);
         res.status(400).json(err);
       });
+    //   We don't need the .sort() method here because we'd only be sorting a single pizza!
   },
   // createPizza
   createPizza({ body }, res) {
--- routes/api/index.js
+++ routes/api/index.js
@@ -2,7 +2,10 @@
 
 const router = require("express").Router();
 const pizzaRoutes = require("./pizza-routes");
+const commentRoutes = require("./comment-routes");
 
+router.use("/comments", commentRoutes);
+router.use("/pizzas", pizzaRoutes);
 // add prefix of `/pizzas` to routes created in `pizza-routes.js`
 router.use("/pizzas", pizzaRoutes);
 
